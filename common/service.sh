c3b6f9f9989b2a226d6363c6d4c533adaef40b4c
Simon Shi <simonsmh@gmail.com>
2017-06-08 19:00:19 +0800

    init

--- common/service.sh
+++ common/service.sh
@@ -1,7 +1,28 @@
 #!/system/bin/sh
-# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
-# This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
+#boot detection
+until [ $(getprop init.svc.bootanim) = "stopped" ]
+do
+sleep 2
+done
+#app_process
+if [ -f /system/bin/app_process64 ]
+then
+ln -sf /system/bin/app_process64 /data/local/tmp/app_process
+else
+ln -sf /system/bin/app_process32 /data/local/tmp/app_process
+fi
+PATH=/data/local/tmp:$PATH
+{ #brevent
+exec $(dumpsys package me.piebridge.brevent | grep legacyNativeLibraryDir | cut -b 28-)/*/libbrevent.so &
+} &
+{ #shizuku
+export CLASSPATH=$(dumpsys package moe.shizuku.privileged.api | grep path | cut -b 13-)
+until $(ifconfig | grep -q tun0) || $(ifconfig | grep -q wlan0) || $(ifconfig | grep -q data0) || [ $i = 24 ]
+do
+sleep 5
+((i++))
+done
+exec app_process /system/bin --nice-name=rikka_server moe.shizuku.server.Server &
+} &
 
-# This script will be executed in late_start service mode
-# More info in the main Magisk thread
--- config.sh
+++ config.sh
@@ -27,11 +27,11 @@
 
 # This will be the folder name under /magisk
 # This should also be the same as the id in your module.prop to prevent confusion
-MODID=template
+MODID=privileged-api-booter
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
-AUTOMOUNT=true
+AUTOMOUNT=false
 
 # Set to true if you need to load system.prop
 PROPFILE=false
@@ -40,7 +40,7 @@ PROPFILE=false
 POSTFSDATA=false
 
 # Set to true if you need late_start service script
-LATESTARTSERVICE=false
+LATESTARTSERVICE=true
 
 ##########################################################################################
 # Installation Message
@@ -50,7 +50,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "  Magisk Privileged Api Booter "
   ui_print "*******************************"
 }
 

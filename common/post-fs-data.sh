e05e05d7acae6504a9374ecccad6ed932bc11a2a
Simon Shi <simonsmh@gmail.com>
2017-12-25 08:43:59 +0800

    Fix supolicy

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -5,8 +5,8 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
-supolicy --live "allow untrusted_app default_android_serice service_manager find"
-supolicy --live "allow untrusted_app su binder { transfer call }"
+supolicy --live "allow * su binder *"
+supolicy --live "allow * default_android_service service_manager find"
 
 ### topjohnwu/magiskpolicy d7a6127273407a32bccf75e70fa7bbe84c14e4f8
 supolicy --live "allow system_server su fifo_file write"
